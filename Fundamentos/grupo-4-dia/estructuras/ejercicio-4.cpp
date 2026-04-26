6b1b780df18c2fe6183f377f8fab1c53c5daffc6
Abel Villaseca <abelvillasecac@gmail.com>
2026-04-26 18:53:28 -0500

    .gitignore

--- Fundamentos/grupo-4-dia/estructuras/ejercicio-4.cpp
+++ Fundamentos/grupo-4-dia/estructuras/ejercicio-4.cpp
@@ -1,3 +1,8 @@
 // Escribe un programa que mediante un menú, gestione los datos de un conjunto de
 // alumnos en un curso determinado. Deberá tener las opciones de ingreso, consulta,
 // modificación, eliminación y listado.
+
+// C => CREATE (Crear)
+// R => READ (Leer)
+// U => UPDATE (Actualizar)
+// D => DELETE (Eliminar)
