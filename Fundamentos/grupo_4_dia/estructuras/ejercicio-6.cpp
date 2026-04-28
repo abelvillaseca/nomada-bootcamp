5545bbe07c2637a997758a866edb580333f25820
Abel Villaseca <abelvillasecac@gmail.com>
2026-04-28 09:51:41 -0500

    ejercicio-6-estucturas

--- Fundamentos/grupo_4_dia/estructuras/ejercicio-6.cpp
+++ Fundamentos/grupo_4_dia/estructuras/ejercicio-6.cpp
@@ -2,3 +2,78 @@
 // el número de páginas de un libro. Implementa un programa que permita ingresar la
 // información de varios libros y luego muestre los libros que tienen más de 300 páginas
 // y que fueron publicados después del año 2000.
+
+#include <iostream>
+
+using namespace std;
+
+struct Libro {
+    string titulo;
+    string autor;
+    int anio;
+    int paginas;
+};
+
+void llenarLibros(Libro libros[], int cantidadLibros) {
+
+    for(int i = 0; i < cantidadLibros; i++){
+      cout<<"\nLibro "<<i + 1<<": "<<endl;
+
+      cout<<"Título: ";
+      getline(cin, libros[i].titulo);
+
+      cout<<"Autor: ";
+      getline(cin, libros[i].autor);
+
+      cout<<"Año de publicación: ";
+      cin>>libros[i].anio;
+
+      cout<<"Número de páginas: ";
+      cin>>libros[i].paginas;
+
+      cin.ignore();
+    }
+}
+
+void mostrarLibro(Libro l) {
+
+    cout<<"\nTítulo: "<<l.titulo<<endl;
+    cout<<"Autor: "<<l.autor<<endl;
+    cout<<"Año de publicación: "<<l.anio<<endl;
+    cout<<"Páginas: "<<l.paginas<<endl;
+}
+
+void mostrarLibrosFiltrados(Libro libros[], int cantidadLibros) {
+
+    bool encontrado = false;
+
+    cout<<"\nLIBROS ENCONTRADOS: "<<endl;
+
+    for(int i = 0; i < cantidadLibros; i++){
+        if(libros[i].paginas > 300 && libros[i].anio > 2000){
+            mostrarLibro(libros[i]);
+            encontrado = true;
+        }
+    }
+
+    if(!encontrado){
+      cout<<"No se encontró ningún libro que cumpla la condición."<<endl;
+    }
+}
+
+int main() {
+
+    int cantidadLibros;
+
+    cout<<"Ingresa la cantidad de libros: ";
+    cin>>cantidadLibros;
+    cin.ignore();
+
+    Libro libros[20];
+
+    llenarLibros(libros, cantidadLibros);
+
+    mostrarLibrosFiltrados(libros, cantidadLibros);
+
+    return 0;
+}
