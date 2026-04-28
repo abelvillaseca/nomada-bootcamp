a4e1dfaa500b7793ff5dfeed9665f9b7e96f4372
Abel Villaseca <abelvillasecac@gmail.com>
2026-04-27 20:27:36 -0500

    bucles ejercicio_1

--- Frontend/JavaScript_II/Semana_1/bucles/while/ejercicio_1.js
+++ Frontend/JavaScript_II/Semana_1/bucles/while/ejercicio_1.js
@@ -1,1 +1,19 @@
 // Usa un bucle while para sumar los números del 1 al 50 e imprimir el resultado final.
+
+// 1 + 2 + 3 + 4 + ... + 50
+
+// acumulador suma = 0
+
+let limite = parseInt(prompt('Ingresa el número límite (máximo 50): ')) // '50' => 50
+
+let suma = 0
+
+let i = 1
+
+while (i <= limite) {
+  suma = suma + i
+  console.log('Paso: i = ', i, '| Suma parcial = ', suma)
+  i++
+}
+
+console.log('La suma total es: ', suma)
---
+++ Frontend/JavaScript_II/Semana_1/bucles/while/ejercicio_2.js
@@ -0,0 +1,1 @@
+// Crea un programa que imprima todos los números pares entre 1 y 20 utilizando un bucle while.
