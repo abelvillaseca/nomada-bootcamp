0b594f51bc76c1205aac594794e537fa9f051f09
Abel Villaseca <abelvillasecac@gmail.com>
2026-04-27 20:35:34 -0500

    bucles ejercicio_2

--- Frontend/JavaScript_II/Semana_1/bucles/while/ejercicio_2.js
+++ Frontend/JavaScript_II/Semana_1/bucles/while/ejercicio_2.js
@@ -1,1 +1,17 @@
-// Crea un programa que imprima todos los números pares entre 1 y 20 utilizando un bucle while.
+// Crea un programa que imprima todos los números pares en un rango ingresado por teclado utilizando un bucle while.
+
+// numero % 2 === 0 => PAR
+
+let inicio = parseInt(prompt('Ingresa el límite inferior: '))
+let fin = parseInt(prompt('Ingresa el límite superior: '))
+
+let i = inicio
+
+while (i <= fin) {
+  if (i % 2 === 0) {
+    console.log('Número par encontrado: ', i)
+  }
+  i++
+}
+
+console.log('Proceso terminado.')
