47609aa0e0a4e65c523f7b7839fd9a141850e967
Abel Villaseca <abelvillasecac@gmail.com>
2026-05-13 20:29:32 -0500

    funciones flecha

--- Frontend/JavaScript_II/Semana_2/ejercicios/js/script.js
+++ Frontend/JavaScript_II/Semana_2/ejercicios/js/script.js
@@ -1,3 +1,4 @@
+// FUNCIÓN DECLARADA
 // function calcularPrecioFinal(precioBase) {
 //   let IGV = precioBase * 0.18
 //   let precioFinal = precioBase + IGV
@@ -7,14 +8,50 @@
 //   }
 // }
 
-const calcularPrecioFinal = function (precioBase) {
+// FUNCIÓN EXPRESADA
+// const calcularPrecioFinal = function (precioBase) {
+//   let IGV = precioBase * 0.18
+//   let precioFinal = precioBase + IGV
+//   return {
+//     IGV: IGV,
+//     precioFinal: precioFinal,
+//   }
+// }
+
+// FUNCIÓN FLECHA
+// const calcularPrecioFinal = (precioBase) => {
+//   let IGV = precioBase * 0.18
+//   let precioFinal = precioBase + IGV
+//   return {
+//     IGV: IGV,
+//     precioFinal: precioFinal,
+//   }
+// }
+
+// FUNCIÓN FLECHA SIMPLIFICADA
+// RETORNO IMPLÍCITO
+// const calcularPrecioFinal = (precioBase) => ({
+//   IGV: precioBase * 0.18,
+//   precioFinal: precioBase + IGV,
+// })
+
+const calcularPrecioFinal = (precioBase) => {
   let IGV = precioBase * 0.18
   let precioFinal = precioBase + IGV
-  return {
-    IGV: IGV,
-    precioFinal: precioFinal,
-  }
+  return precioFinal
 }
+
+let total1 = calcularPrecioFinal(100)
+let total2 = calcularPrecioFinal(250)
+let total3 = calcularPrecioFinal(50)
+
+console.log(`Precio final 1: ${total1.toFixed(2)}`)
+console.log(`Precio final 2: ${total2.toFixed(2)}`)
+console.log(`Precio final 3: ${total3.toFixed(2)}`)
+
+let sumaTotal = total1 + total2 + total3
+
+console.log(`Suma total: ${sumaTotal.toFixed(2)}`)
 
 function procesar() {
   let precioBase = parseFloat(document.getElementById('precioBase').value)
