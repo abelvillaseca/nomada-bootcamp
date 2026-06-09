8bba429c914200e89c51c66de3ea589cf0c096f6
Abel Villaseca <abelvillasecac@gmail.com>
2026-06-08 21:07:47 -0500

    metodos comunes ejercicios

---
+++ Frontend/JavaScript_III/dos.js
@@ -0,0 +1,5 @@
+// Convierte arreglos a texto y viceversa con join y split:
+
+// Convierte ["2025", "12", "31"] en la fecha "2025/12/31" con join
+// Convierte "rojo, verde, azul" en un arreglo con split
+// Une los dos arreglos en uno con concat
---
+++ Frontend/JavaScript_III/uno.js
@@ -0,0 +1,5 @@
+// Usa indexOf e includes para buscar dentro de un arreglo:
+
+// - Arreglo de frutas con al menos 5 frutas
+// - Función buscar(fruta) que retorne la posición o el texto "no encontrado" si no está.
+// - Usa includes para verificar la existencia antes de buscar la posición.
