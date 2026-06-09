d0de256dae333405f0a6a374a684ed95181a4fe8
Abel Villaseca <abelvillasecac@gmail.com>
2026-06-08 21:25:10 -0500

    ejercicio 1

--- Frontend/JavaScript_III/uno.js
+++ Frontend/JavaScript_III/uno.js
@@ -3,3 +3,28 @@
 // - Arreglo de frutas con al menos 5 frutas
 // - Función buscar(fruta) que retorne la posición o el texto "no encontrado" si no está.
 // - Usa includes para verificar la existencia antes de buscar la posición.
+
+let frutas = [
+    'manzana',
+    'pera',
+    'mango',
+    'uva',
+    'plátano'
+];
+
+function buscar(fruta) {
+    if (frutas.includes(fruta)) {
+        let posicion = frutas.indexOf(fruta);
+        return posicion;
+    } else {
+        return 'No encontrado';
+    }
+}
+
+let resultado = buscar('taperiba');
+
+if (resultado != 'No encontrado') {
+    console.log(`La fruta se encuentra en la posición ${resultado}`);
+} else {
+    console.log(resultado);
+}
