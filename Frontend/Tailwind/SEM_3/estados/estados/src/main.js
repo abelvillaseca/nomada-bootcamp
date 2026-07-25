8942301c78380ca04ba662bb72be1ab7ce9ffb31
Abel Villaseca <abelvillasecac@gmail.com>
2026-07-24 22:25:06 -0500

    ejercicio que no hicieron los alumnos

--- Frontend/Tailwind/SEM_3/estados/estados/src/main.js
+++ Frontend/Tailwind/SEM_3/estados/estados/src/main.js
@@ -1,1 +1,15 @@
 import './style.css'
+
+const html = document.documentElement
+
+const themeBtn = document.querySelector('#themeBtn')
+const menuBtn = document.querySelector('#menuBtn')
+const mobileMenu = document.querySelector('#mobileMenu')
+
+themeBtn.addEventListener('click', () => {
+    html.classList.toggle('dark')
+})
+
+menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden')
+})
