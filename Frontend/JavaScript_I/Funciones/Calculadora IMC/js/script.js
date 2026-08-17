87612b9aa547108a09c026d3338f24a13af76dbf
Abel Villaseca <abelvillasecac@gmail.com>
2026-08-17 17:34:03 -0500

    IMC calculadora

--- Frontend/JavaScript_I/Funciones/Calculadora IMC/js/script.js
+++ Frontend/JavaScript_I/Funciones/Calculadora IMC/js/script.js
@@ -2,3 +2,32 @@
 // Debe devolver el Índice de Masa Corporal (IMC), calculado como IMC=peso/altura al cuadrado. Asegúrate de devolver
 // el resultado redondeado a dos decimales.
 
+// peso = 70
+// altura = 1.75
+// IMC = 70 / (1.75 * 1.75)
+// IMC = 22.85...
+
+const calcularIMC = (peso, altura) => {
+    const imc = peso / Math.pow(altura, 2)
+    return Number(imc.toFixed(2))
+}
+
+const formulario = document.querySelector('#imcForm')
+const inputPeso = document.querySelector('#peso')
+const inputAltura = document.querySelector('#altura')
+const valorIMC = document.querySelector('#valorIMC')
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const peso = Number(inputPeso.value)
+    const altura = Number(inputAltura.value)
+
+    if (peso <= 0 || altura <= 0) {
+        valorIMC.textContent = 'Datos inválidos'
+        return
+    }
+
+    const resultado = calcularIMC(peso, altura)
+
+    valorIMC.textContent = resultado
+})
