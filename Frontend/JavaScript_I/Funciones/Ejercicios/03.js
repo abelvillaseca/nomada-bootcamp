93420f82956ccecb51668adbd0a0954da20ae1f8
Abel Villaseca <abelvillasecac@gmail.com>
2026-08-24 17:47:49 -0500

    ejercicio3

--- Frontend/JavaScript_I/Funciones/Ejercicios/03.js
+++ Frontend/JavaScript_I/Funciones/Ejercicios/03.js
@@ -10,3 +10,34 @@
 // Cuántos números impares existen.
 // Cuántos son múltiplos de 5.
 // La suma de todos los números.
+
+function analizarNumeros(inicio, fin) {
+    let contadorPares = 0
+    let contadorImpares = 0
+    let constadorMultiplos = 0
+    let suma = 0
+
+    for (let numero = inicio; numero <= fin; numero++) {
+        suma += numero // suma = suma + numero
+        if (numero % 2 === 0) {
+            contadorPares++
+        } else {
+            contadorImpares++
+        }
+        if (numero % 5 === 0) {
+            constadorMultiplos++
+        }
+    }
+    return `
+        Números pares: ${contadorPares}
+        Números impares: ${contadorImpares}
+        Números múltiplos de 5: ${constadorMultiplos}
+        Suma: ${suma}
+    `
+}
+
+const inicio = 1
+const fin = 10
+
+const resultado = analizarNumeros(inicio, fin)
+console.log(resultado)
