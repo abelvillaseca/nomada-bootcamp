1d532c3618c3e641362f506e2b4b274713ae4f1f
Abel Villaseca <abelvillasecac@gmail.com>
2026-08-25 08:33:08 -0500

    cantidad primos

--- Frontend/JavaScript_I/Funciones/Ejercicios/05.js
+++ Frontend/JavaScript_I/Funciones/Ejercicios/05.js
@@ -22,3 +22,42 @@
 // contarPrimos(inicio, fin)
 
 // que determine cuántos números primos existen dentro de un rango.
+
+function esPrimo(numero) {
+    if (numero < 2) {
+        return false
+    }
+
+    for (let divisor = 2; divisor < numero; divisor++) {
+        if (numero % divisor === 0) {
+            // 10 % 2 === 0 (divisible)
+            return false
+        }
+    }
+    return true
+}
+
+function contarPrimos(inicio, fin) {
+    let contador = 0
+    for (let numero = inicio; numero <= fin; numero++) {
+        if (esPrimo(numero)) {
+            contador++
+        }
+    }
+    return contador
+}
+
+const inicio = 1
+const fin = 10
+
+for (let numero = inicio; numero <= fin; numero++) {
+    if (esPrimo(numero)) {
+        console.log(`${numero} es primo`)
+    } else {
+        console.log(`${numero} no es primo`)
+    }
+}
+
+const cantidadPrimos = contarPrimos(inicio, fin)
+
+console.log(`Existen ${cantidadPrimos} números primos entre ${inicio} y ${fin}`)
