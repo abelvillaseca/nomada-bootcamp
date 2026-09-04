5caad6e23ff660fde329a3ebd622716af288a160
Abel Villaseca <abelvillasecac@gmail.com>
2026-09-04 09:56:26 -0500

    manejo del DOM ejercicios

--- Frontend/JavaScript_I/DOM/ejercicios/js/app.js
+++ Frontend/JavaScript_I/DOM/ejercicios/js/app.js
@@ -4,3 +4,20 @@ boton.addEventListener('click', function () {
     const input = document.getElementById('nombre')
     input.value = 'Carlos'
 })
+
+const botnImg = document.getElementById('btnImagen')
+
+botnImg.addEventListener('click', function () {
+    const img = document.getElementById('imagen')
+    img.src = 'images/dos.png'
+    // img.setAttribute('src', 'images/dos.png')
+})
+
+const botonContar = document.getElementById('btnContar')
+const resultado = document.getElementById('contador')
+
+botonContar.addEventListener('click', function () {
+    const elementos = document.getElementsByTagName('li')
+    const cantidad = elementos.length
+    resultado.textContent = `El documento tiene ${cantidad} elementos`
+})
