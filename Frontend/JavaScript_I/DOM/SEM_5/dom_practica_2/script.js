ae81b3c6c298cc5d9aa154b35809ca92bb03364e
Abel Villaseca <abelvillasecac@gmail.com>
2026-09-09 16:18:37 -0500

    outerHTML

--- Frontend/JavaScript_I/DOM/SEM_5/dom_practica_2/script.js
+++ Frontend/JavaScript_I/DOM/SEM_5/dom_practica_2/script.js
@@ -15,3 +15,17 @@ const mensaje = document.getElementById("mensaje");
 botonColor.addEventListener("click", () => {
   mensaje.innerHTML = `<span style="color: blue;">Este es un texto con color azul.</span>`;
 });
+
+// Crea un párrafo con un mensaje y un botón que lo reemplace por una imagen utilizando outerHTML.
+const botonReemplazar = document.getElementById("btnReemplazar");
+const mensajeImagen = document.getElementById("mensajeImagen");
+
+botonReemplazar.addEventListener("click", function () {
+  mensajeImagen.outerHTML = `
+    <img
+      class="imagen-reemplazo"
+      src="https://elcomercio.pe/resizer/v2/KRKOINCP7JGR7KWT4HNBDSJPDA.jpg?auth=8a023522ad969a505c109d936b7e56d4948d7b7e88b9df5ec736e46b8db0d567&width=1200&height=800&quality=75&smart=true"
+      alt="Imagen de Rick Sánchez"
+    >
+  `;
+});
