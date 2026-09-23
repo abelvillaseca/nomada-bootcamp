8aa1f23f1db8929f3c4b36ac10fb007cc37c0f64
Abel Villaseca <abelvillasecac@gmail.com>
2026-09-23 17:51:46 -0500

    ejercicios 2

--- Frontend/JavaScript_I/DOM/SEM_5/POO/ejercicio_04.js
+++ Frontend/JavaScript_I/DOM/SEM_5/POO/ejercicio_04.js
@@ -1,1 +1,13 @@
 // Crea una clase ConversorMoneda con un método estático que convierta dólares a euros.
+
+class ConversorMoneda {
+  static dolaresAEuros(dolares) {
+    const tipoCambio = 0.85;
+    const euros = dolares * tipoCambio;
+    return euros;
+  }
+}
+
+const resultado = ConversorMoneda.dolaresAEuros(100);
+
+console.log(`100 dólares equivalen a: ${resultado} euros`);
--- Frontend/JavaScript_I/DOM/SEM_5/POO/ejercicio_05.js
+++ Frontend/JavaScript_I/DOM/SEM_5/POO/ejercicio_05.js
@@ -1,1 +1,35 @@
 // Crea una clase Persona y otra Direccion. Haz que una Persona pueda tener una Direccion asociada.
+
+class Direccion {
+  constructor(calle, ciudad, pais) {
+    this.calle = calle;
+    this.ciudad = ciudad;
+    this.pais = pais;
+  }
+
+  mostrarDireccion() {
+    return `${this.calle}, ${this.ciudad}, ${this.pais}`;
+  }
+}
+
+class Persona {
+  constructor(nombre, direccion) {
+    this.nombre = nombre;
+    this.direccion = direccion;
+  }
+
+  mostrarInformacion() {
+    console.log(`Nombre: ${this.nombre}`);
+    console.log(`Dirección: ${this.direccion.mostrarDireccion()}`);
+  }
+}
+
+const direccion = new Direccion(
+  "Av. Mártires de Uchuracay",
+  "Cajamarca",
+  "Perú",
+);
+
+const persona = new Persona("Edgard", direccion);
+
+persona.mostrarInformacion();
